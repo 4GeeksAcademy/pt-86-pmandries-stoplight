9d37dcf143103358ecde5bf12f5ddbeb0d901466
pmandries <pandries@aol.com>
2025-10-25 01:07:15 +0000

    first commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -6,21 +6,15 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
+		<div className="text-center text-light container-fluid display-flex">
+            <div className = "container mt-5 bg-secondary" id = "trafficTop">Top</div>
+			<div className = "container mt-5 bg-black" id = "container">
+				<div className = "container rounded-circle bg-danger">Red</div>
+				<div className = "container radius-50 bg-warning">Yellow</div>
+				<div className = "container radius-50 bg-success">Green</div>
+			</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
 		</div>
 	);
 };
